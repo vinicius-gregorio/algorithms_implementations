5d375d4e47884dfd0b5fc826d7344f7bda66adb0
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-09-13 09:57:55 -0300

    initial commit

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/vinicius-gregorio/algorithms_implementations
+
+go 1.21.1
---
+++ go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/vinicius-gregorio/algorithms_implementations/search"
+)
+
+func main() {
+
+	fmt.Println("Program started")
+	search.Search()
+}
